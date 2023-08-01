1dfb0485e129ed7c3d718467181c104ebb69ac34
Max Jendruk <code@jendruk.com>
2023-08-01 08:30:11 +0200

    add option to disable pick_one overwrite

--- lua/telescope/_extensions/dap.lua
+++ lua/telescope/_extensions/dap.lua
@@ -231,35 +231,39 @@ local frames = function(opts)
 end
 
 return telescope.register_extension {
-  setup = function()
+  setup = function(ext_config)
     vim.cmd [[ highlight default link NvimDapSubtleFrame Comment ]]
 
-    require('dap.ui').pick_one = function(items, prompt, label_fn, cb)
-      local opts = {}
-      pickers.new(opts, {
-        prompt_title = prompt,
-        finder    = finders.new_table {
-          results = items,
-          entry_maker = function(entry)
-            return {
-              value = entry,
-              display = label_fn(entry),
-              ordinal = label_fn(entry),
-            }
+    overwrite_pick_one = vim.F.if_nil(ext_config.overwrite_pick_one, false)
+
+    if overwrite_pick_one then
+      require('dap.ui').pick_one = function(items, prompt, label_fn, cb)
+        local opts = {}
+        pickers.new(opts, {
+          prompt_title = prompt,
+          finder    = finders.new_table {
+            results = items,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = label_fn(entry),
+                ordinal = label_fn(entry),
+              }
+            end,
+          },
+          sorter = conf.generic_sorter(opts),
+          attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+              local selection = action_state.get_selected_entry()
+              actions.close(prompt_bufnr)
+
+              cb(selection.value)
+            end)
+
+            return true
           end,
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr)
-          actions.select_default:replace(function()
-            local selection = action_state.get_selected_entry()
-            actions.close(prompt_bufnr)
-
-            cb(selection.value)
-          end)
-
-          return true
-        end,
-      }):find()
+        }):find()
+      end
     end
   end,
   exports = {

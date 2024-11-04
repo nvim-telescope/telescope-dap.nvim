783366bd6c1e7fa0a5c59c07db37f49c805a28df
Andreas Schneider <asn@cryptomilk.org>
2024-11-04 21:27:48 +0100

    Do not overwrite nvim-dap pick_one (#24)
    This fixes telescope-dap.nvim with nvim-dap 0.8. In order to get a nicer
    picker for, use something which overwrites vim.ui.select! For example:
    
    * https://github.com/stevearc/dressing.nvim
    * https://github.com/mfussenegger/nvim-fzy
    * https://github.com/nvim-telescope/telescope-ui-select.nvim

--- lua/telescope/_extensions/dap.lua
+++ lua/telescope/_extensions/dap.lua
@@ -236,34 +236,6 @@ end
 return telescope.register_extension {
   setup = function()
     vim.cmd [[ highlight default link NvimDapSubtleFrame Comment ]]
-
-    require('dap.ui').pick_one = function(items, prompt, label_fn, cb)
-      local opts = {}
-      pickers.new(opts, {
-        prompt_title = prompt,
-        finder    = finders.new_table {
-          results = items,
-          entry_maker = function(entry)
-            return {
-              value = entry,
-              display = label_fn(entry),
-              ordinal = label_fn(entry),
-            }
-          end,
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr)
-          actions.select_default:replace(function()
-            local selection = action_state.get_selected_entry()
-            actions.close(prompt_bufnr)
-
-            cb(selection.value)
-          end)
-
-          return true
-        end,
-      }):find()
-    end
   end,
   exports = {
     commands = commands,
